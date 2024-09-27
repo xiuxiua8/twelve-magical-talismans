a519201d88691bdff5348a7812c8dbb32168ad7e
xiuxiua8 <1831894878@qq.com>
2024-09-27 12:56:01 +0800

    12 talismans and spawn func

--- main.lua
+++ main.lua
@@ -4,19 +4,33 @@ local game = Game()
 --Talismans.COLLECTIBLE_ROOSTER_TALISMAN = Isaac.GetItemIdByName("the Rooster Talisman")
 
 local TalismanId = {
+    RAT = Isaac.GetItemIdByName("the Rat Talisman"),
     OX = Isaac.GetItemIdByName("the Ox Talisman"),
+    TIGER = Isaac.GetItemIdByName("the Tiger Talisman"),
     RABBIT = Isaac.GetItemIdByName("the Rabbit Talisman"),
     DRAGON = Isaac.GetItemIdByName("the Dragon Talisman"),
-    SNACK = Isaac.GetItemIdByName("the Snack Talisman"),
-    ROOSTER = Isaac.GetItemIdByName("the Rooster Talisman")
+    SNAKE = Isaac.GetItemIdByName("the Snake Talisman"),
+    HORSE = Isaac.GetItemIdByName("the Horse Talisman"),
+    SHEEP = Isaac.GetItemIdByName("the Sheep Talisman"),
+    MONKEY = Isaac.GetItemIdByName("the Monkey Talisman"),
+    ROOSTER = Isaac.GetItemIdByName("the Rooster Talisman"),
+    DOG = Isaac.GetItemIdByName("the Dog Talisman"),
+    PIG = Isaac.GetItemIdByName("the Pig Talisman")
 }
 
 local HasTalisman = {
-    ROOSTER = false,
+    RAT = false,
     OX = false,
+    TIGER = false,
     RABBIT = false,
-    SNACK = false,
-    DRAGON = false
+    DRAGON = false,
+    SNAKE = false,
+    HORSE = false,
+    SHEEP = false,
+    MONKEY = false,
+    ROOSTER = false,
+    DOG = false,
+    PIG = false
 }
 
 local Bonus = {
@@ -34,15 +48,19 @@ local DRAGON = {
 
 --Update the inventory
 local function UpdateTalismans(player) 
-    HasTalisman.ROOSTER = player:HasCollectible(TalismanId.ROOSTER)
+    HasTalisman.RAT = player:HasCollectible(TalismanId.RAT)
     HasTalisman.OX = player:HasCollectible(TalismanId.OX)
+    HasTalisman.TIGER = player:HasCollectible(TalismanId.TIGER)
     HasTalisman.RABBIT = player:HasCollectible(TalismanId.RABBIT)
-    HasTalisman.SNACK = player:HasCollectible(TalismanId.SNACK)
     HasTalisman.DRAGON = player:HasCollectible(TalismanId.DRAGON)
+    HasTalisman.SNAKE = player:HasCollectible(TalismanId.SNAKE)
+    HasTalisman.HORSE = player:HasCollectible(TalismanId.HORSE)
+    HasTalisman.SHEEP = player:HasCollectible(TalismanId.SHEEP)
+    HasTalisman.MONKEY = player:HasCollectible(TalismanId.MONKEY)
+    HasTalisman.ROOSTER = player:HasCollectible(TalismanId.ROOSTER)
+    HasTalisman.DOG = player:HasCollectible(TalismanId.DOG)
+    HasTalisman.PIG = player:HasCollectible(TalismanId.PIG)
 end
-
-
-
 
 --when the run starts or continues
 function Talismans:onPlayerInit(player)
@@ -51,6 +69,26 @@ end
 Talismans:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, Talismans.onPlayerInit)
 
 
+function Talismans:spawnTalismans(player)
+    if game:GetFrameCount() == 1 then
+        Isaac.DebugString("Rooster Talisman ID: " .. tostring(TalismanId.ROOSTER))
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.RAT, Vector(170,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.OX, Vector(220,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.TIGER, Vector(270,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.RABBIT, Vector(370,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.DRAGON, Vector(420,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.SNAKE, Vector(470,250), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.HORSE, Vector(170,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.SHEEP, Vector(220,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.MONKEY, Vector(270,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.ROOSTER, Vector(370,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.DOG, Vector(420,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.PIG, Vector(470,300), Vector(0,0), nil)
+        if player:GetName() == "Isaac" then
+            player:AddCollectible(TalismanId.OX, 0, true)
+        end 
+    end
+end
 
 --when passive effects should update
 function Talismans:onUpdate(player)
@@ -60,17 +98,7 @@ function Talismans:onUpdate(player)
         Isaac.DebugString("Mod data does not exist for Talismans mod.")
     end
     -- Begining of run initializations
-    if game:GetFrameCount() == 1 then
-        Isaac.DebugString("Rooster Talisman ID: " .. tostring(TalismanId.ROOSTER))
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.ROOSTER, Vector(320,300), Vector(0,0), nil)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.OX, Vector(270,300), Vector(0,0), nil)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.RABBIT, Vector(370,300), Vector(0,0), nil)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.SNACK, Vector(220,300), Vector(0,0), nil)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TalismanId.DRAGON, Vector(420,300), Vector(0,0), nil)
-        if player:GetName() == "Isaac" then
-            player:AddCollectible(TalismanId.OX, 0, true)
-        end 
-    end
+    Talismans.spawnTalismans(player)
     UpdateTalismans(player)
 end
 
